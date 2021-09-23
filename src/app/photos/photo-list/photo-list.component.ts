2d012446772bcc248ce35560f4a6a0010cbbe438
Eber <eber20@uol.com.br>
2021-09-23 18:30:41 -0300

    LoadButton com paginação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 
 @Component({
@@ -16,19 +17,37 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos : Photo[] = [];
   filter : string = ''
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
 
-  constructor(private activatedRouter : ActivatedRoute){}
+  constructor(private activatedRouter : ActivatedRoute, private photoService : PhotoService){}
   
   ngOnInit(){
     this.photos = this.activatedRouter.snapshot.data.photos;
     this.debounce
     .pipe(debounceTime(300))
     .subscribe(filter => this.filter = filter);
+    this.userName = this.activatedRouter.snapshot.params.userName;
+    console.log(`------- ${this.userName}`);
+
   }
   
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
   }
   
+  load() {
+    
+    //currentPage começa de 2, pois o 'resolver tratou o 1 carregamento'
+
+    this.photoService
+        .listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+            this.photos = this.photos.concat(photos);
+            if(!photos.length) this.hasMore = false;
+        });
+}
+
 }
