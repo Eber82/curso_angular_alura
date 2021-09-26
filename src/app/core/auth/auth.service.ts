cb4472f0598d0b0128c9698a58dc3df243554888
Eber <eber20@uol.com.br>
2021-09-26 19:06:51 -0300

    comunicacao com API para logar

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private API_URL = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+  
+  autenthicate(userName : string, password : string){
+    return this.http.post(
+          this.API_URL + '/user/login',
+          {
+            userName : userName,
+            password : password
+          }
+    )
+  }
+}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth/auth.service";
 
 @Component({
     templateUrl : './signin.component.html'
@@ -8,7 +9,7 @@ export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(private formBuilder: FormBuilder, private authService : AuthService) { }
 
     ngOnInit(): void {
         
@@ -17,4 +18,21 @@ export class SignInComponent implements OnInit {
             password : ['', Validators.required]
         });
     }
+
+    logar(){
+        
+        const userName  = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        
+        this.authService
+        .autenthicate(userName, password)
+        .subscribe(
+            () => console.log('vai logar'),
+            err => {
+                console.log(err),
+                this.loginForm.reset();
+            }
+        );
+    }
+
 }
