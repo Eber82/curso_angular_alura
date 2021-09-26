1ba347334bfd7bc28be0dcf8b5b49c1e65e62d8f
Eber <eber20@uol.com.br>
2021-09-26 14:11:43 -0300

    Componentizando a messagem de campo obrigatorio

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { SignInComponent } from "./signin/signin.component";
+import { VMessageModule } from '../shared/vmessage/vmessage.module';
 
 @NgModule({
     declarations:[SignInComponent],
-    imports : [ReactiveFormsModule, CommonModule]
+    imports : [ReactiveFormsModule, CommonModule, VMessageModule]
 })
 
 export class HomeModule{
