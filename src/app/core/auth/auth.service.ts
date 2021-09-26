e35a683e2ce473186872cac988dbc0ef76e9c299
Eber <eber20@uol.com.br>
2021-09-26 20:27:21 -0300

    Acessando valores do header do response

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { tap } from 'rxjs/operators';
+ 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  private API_URL = 'http://localhost:3000';
-
   constructor(private http: HttpClient) { }
+
+  private API_URL = 'http://localhost:3000';
   
   autenthicate(userName : string, password : string){
-    return this.http.post(
-          this.API_URL + '/user/login',
-          {
-            userName : userName,
-            password : password
-          }
-    )
+    
+      return this.http
+          .post(this.API_URL + '/user/login', 
+                { userName : userName, password : password },
+                {observe : 'response'}) //3º parametro para 'liberar' o acesso ao response da requisição
+          .pipe(tap(res => {
+              const authToken = res.headers.get('x-access-token');
+              console.log(`User ${userName} authenticated with token ${authToken}`);
+          }))
   }
 }
